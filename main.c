530196b7f21e2942c88656a6fcd83e582cceab94
João Vitor Farias <102336880+joaovfarias@users.noreply.github.com>
2023-03-26 15:10:46 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,3 @@
-/*
-Faça uma agenda capaz de incluir, apagar, buscar e listar
-quantas pessoas o usuário desejar, porém, toda a informação
-incluída na agenda deve ficar num único lugar chamado: “void
-*pBuffer”.
-Não pergunte para o usuário quantas pessoas ele vai incluir.
-Não pode alocar espaço para mais pessoas do que o
-necessário.
-Cada pessoa tem nome[10], idade e telefone.
-
-Continuar a sua implementação da agenda (exercício 3) semana 1 dentro dos mesmo parâmetros, mas incluir o seguinte.
-Nenhuma variável pode ser declarada em todo o programa, somente ponteiros. Todos os dados do programa devem ser guardados dentro do pBuffer.
-Nem mesmo como parâmetro de função. Só ponteiros que apontam para dentro do pBuffer.
-Exemplo do que não pode: int c; char a; int v[10];  void Funcao(int parametro)
-Não pode usar struct em todo o programa.
-Usar fila ordenada (heap) para armazenar as pessoas em ordem alfabética sempre que o usuário incluir uma nova pessoa.
-Implementar a base de dados da agenda usando lista duplamente ligada
-Somente essa base de dados pode ficar fora do buffer principal, ou seja, pode usar um malloc para cada nodo.
-Seguir as orientações elaborados pelo monitor para código!
-Entregar link de um vídeo explicando o código e mostrando alguns testes e ao final o resultado do DrMemory.
-Vou perguntar no 1:1 sobre a sua implementação. Posso pedir para fazer alguma modificação no código na nossa conversa!
-*/
-
 // Pbuffer Bytes : 1-4 = call;
 //                 5-14 = Nome para ser buscado;
 //                 15-24 = Nome;
